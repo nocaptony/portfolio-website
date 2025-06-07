41d7d08f0a440467c0840aafc5566632233b2397
Tony <artionkaraj@gmail.com>
2025-06-06 22:05:54 -0400

    general qol updates

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -101,4 +101,4 @@ export default function Contact() {
       <ToastContainer position="bottom-right" />
     </div>
   );
-} 
+}
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="relative flex flex-col w-full items-center justify-center first-line:bottom-0 bg-[#1B1B1B]">
-      <div className="">© Copyright 2023 by nocaptony </div>
+      <div className="">© Copyright 2025 by nocaptony </div>
       <div className="">
         Powered by <span className="text-[#4ac4a3]">Next.js</span> and{" "}
         <span className="text-[#4ac4a3]">TailwindCSS</span>. Hosted on
